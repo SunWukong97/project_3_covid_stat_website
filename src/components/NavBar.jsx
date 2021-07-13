0b09e6992f12374144a2bd20f26204d52927b577
SunWukong97 <34043095+SunWukong97@users.noreply.github.com>
2021-07-13 19:19:50 -0400

    Changed the logo
    changed the logo from the default react one to the one I made

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./NavBar.css";
+import logo from "../images/logo.png";
 class NavBar extends Component {
   render() {
     return (
       <nav className="navbar">
         <div className="navbar-container">
-          <a href="#" className="navbar-logo">
+          <a href="index.html" className="navbar-logo">
+            <img src={logo} alt="the logo" />
             COVID STATS CAN
           </a>
         </div>
