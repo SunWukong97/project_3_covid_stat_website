c7dabf6d18a9e64a72c2b337676cf93734ecab82
SunWukong97 <34043095+SunWukong97@users.noreply.github.com>
2021-07-16 22:08:13 -0400

    created about section component

---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./AboutSection.css";
+function AboutSection() {
+  return (
+    <div className="about-container">
+      <h2>About</h2>
+      <p>
+        This is a personal project where I create a react app along with the
+        usage of the <a href="https://opencovid.ca/api/">Open Covid API</a> to
+        create a visual representation of Covid stats in Canada.
+      </p>
+    </div>
+  );
+}
+
+export default AboutSection;
