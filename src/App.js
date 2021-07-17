5f909c106994d5d0aa19ec11cfa8f14f66b14e6d
SunWukong97 <34043095+SunWukong97@users.noreply.github.com>
2021-07-16 22:08:00 -0400

    Added about section component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 import NavBar from "./components/NavBar.jsx";
 import HeroSection from "./components/HeroSection";
+import AboutSection from "./components/AboutSection";
 function App() {
   return (
     <React.Fragment>
       <NavBar />
       <HeroSection />
+      <AboutSection />
     </React.Fragment>
   );
 }
