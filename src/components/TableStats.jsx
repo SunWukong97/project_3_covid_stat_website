18a95172e5e9974f88f58ce075fa6b8ffe140aee
SunWukong97 <34043095+ThomasNLy@users.noreply.github.com>
2021-11-23 12:38:32 -0500

    Changed to functional component and removed api call from it.
    TableStats.jsx will now be a controlled component by its parent component App.js.
    
    Added additional features such as showing the entire country's stats.

--- src/components/TableStats.jsx
+++ src/components/TableStats.jsx
@@ -1,135 +1,144 @@
-import React, { Component } from "react";
+import React from "react";
 import StatusBar from "./StatusBar";
 import "./TableStats.css";
-class TableStats extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dataSet1: null,
-      dataSet2: null,
-      isLoading: true,
-      date: null,
-    };
-  }
-  componentDidMount() {
-    let todayDate = new Date();
-    let yesterdayDate = new Date(todayDate);
-    yesterdayDate.setDate(yesterdayDate.getDate() - 1);
-    let formatDate = yesterdayDate.toISOString().slice(0, 10);
-    const apiUrl1 = `https://api.opencovid.ca/summary?date=${formatDate}`;
-    const apiUrl2 = "https://api.opencovid.ca/other?stat=prov";
+function TableStats(props) {
+  const { data1, data2, data3, date } = props;
+  let tableData;
+  let canadaCovidStat = data3[0];
 
-    let apiUrls = [apiUrl1, apiUrl2];
-
-    Promise.all(
-      apiUrls.map((urlIndex) =>
-        fetch(urlIndex).then((response) => parseJson(response))
-      )
-    ).then((data) => {
-      console.log("Success", data[0]);
-      console.log("Success", data[1]);
-      this.setState({
-        dataSet1: data[0].summary,
-        dataSet2: data[1].prov,
-        isLoading: false,
-        date: formatDate,
-      });
+  let canadaPopulation = calculateCountryPop(data2);
+  //needed as it maybe called before api call has been completed
+  tableData = data1.map((provCovidStat) => {
+    return data2.map((provInfo) => {
+      if (
+        provCovidStat.province === provInfo.province &&
+        provCovidStat.province !== "Repatriated"
+      ) {
+        let provinceName;
+        if (provInfo.province === "BC") {
+          provinceName = "British Columbia";
+        } else if (provInfo.province === "NL") {
+          provinceName = "Newfoundland and Labrador";
+        } else {
+          provinceName = provInfo.province;
+        }
+        return (
+          <tr key={provCovidStat.province} className="table-row">
+            <td className="table-cell province-table-cell">
+              <p className="provinces-names">{provinceName}</p>
+            </td>
+            <td className="table-cell">
+              <StatusBar
+                data={provCovidStat.active_cases}
+                data2={provInfo.pop}
+                barType="cases"
+              />
+            </td>
+            <td className="table-cell">
+              <StatusBar
+                data={provCovidStat.cumulative_recovered}
+                data2={provCovidStat.cumulative_cases}
+                barType="recovered"
+              />
+            </td>
+            <td className="table-cell">
+              <StatusBar
+                data={provCovidStat.cumulative_deaths}
+                data2={provCovidStat.cumulative_cases}
+                barType="mortality"
+              />
+            </td>
+          </tr>
+        );
+      }
+      return null;
     });
-  }
+  });
 
-  render() {
-    let tableData;
-    let date;
-    //needed as it maybe called before api call has been completed
-    if (!this.state.isLoading && this.state.dataSet1.length !== 0) {
-      date = this.state.date;
+  return (
+    <React.Fragment>
+      <h1 className="date">{date}</h1>
+      <div className="table-container">
+        <table className="country-stats">
+          <tbody>
+            <tr className="table-header">
+              <td className="table-cell">
+                <h2>&nbsp;</h2>
+              </td>
+              <td className="table-cell">
+                <h2>Number Of Active Cases</h2>
+              </td>
+              <td className="table-cell">
+                <h2>Recovered</h2>
+              </td>
+              <td className="table-cell">
+                <h2>Mortality Rate</h2>
+              </td>
+            </tr>
+            <tr className="table-row">
+              <td className="table-cell province-table-cell">
+                <p className="provinces-names">{canadaCovidStat.province}</p>
+              </td>
+              <td className="table-cell">
+                <StatusBar
+                  data={canadaCovidStat.active_cases}
+                  data2={canadaPopulation}
+                  barType="cases"
+                />
+              </td>
+              <td className="table-cell">
+                <StatusBar
+                  data={canadaCovidStat.cumulative_recovered}
+                  data2={canadaCovidStat.cumulative_cases}
+                  barType="recovered"
+                />
+              </td>
+              <td className="table-cell">
+                <StatusBar
+                  data={canadaCovidStat.cumulative_deaths}
+                  data2={canadaCovidStat.cumulative_cases}
+                  barType="mortality"
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <table>
+          <tbody>
+            <tr className="table-header">
+              <td className="table-cell">
+                <h2>Province/Territories</h2>
+              </td>
+              <td className="table-cell">
+                <h2>Number Of Active Cases</h2>
+              </td>
+              <td className="table-cell">
+                <h2>Recovered</h2>
+              </td>
+              <td className="table-cell">
+                <h2>Mortality Rate</h2>
+              </td>
+            </tr>
 
-      tableData = this.state.dataSet1.map((provCovidStat) => {
-        return this.state.dataSet2.map((provInfo) => {
-          if (
-            provCovidStat.province === provInfo.province &&
-            provCovidStat.province !== "Repatriated"
-          ) {
-            let provinceName;
-            if (provInfo.province === "BC") {
-              provinceName = "British Columbia";
-            } else if (provInfo.province === "NL") {
-              provinceName = "Newfoundland and Labrador";
-            } else {
-              provinceName = provInfo.province;
-            }
-            return (
-              <tr key={provCovidStat.province} className="table-row">
-                <td className="table-cell province-table-cell">
-                  <p className="provinces-names">{provinceName}</p>
-                </td>
-                <td className="table-cell">
-                  <StatusBar
-                    data={provCovidStat.active_cases}
-                    data2={provInfo.pop}
-                    barType="cases"
-                  />
-                </td>
-                <td className="table-cell">
-                  <StatusBar
-                    data={provCovidStat.cumulative_recovered}
-                    data2={provCovidStat.cumulative_cases}
-                    barType="recovered"
-                  />
-                </td>
-                <td className="table-cell">
-                  <StatusBar
-                    data={provCovidStat.cumulative_deaths}
-                    data2={provCovidStat.cumulative_cases}
-                    barType="mortality"
-                  />
-                </td>
-              </tr>
-            );
-          }
-          return null;
-        });
-      });
-    } else {
-      tableData = (
-        <tr>
-          <td colSpan="4" align="center">
-            <p className="error">Data is Currently Unavailable</p>
-          </td>
-        </tr>
-      );
-    }
-
-    return (
-      <React.Fragment>
-        <h1 className="date">{date}</h1>
-        <div className="table-container">
-          <table>
-            <tbody>
-              <tr className="table-header">
-                <td className="table-cell">
-                  <h2>Province/Territories</h2>
-                </td>
-                <td className="table-cell">
-                  <h2>Number Of Active Cases</h2>
-                </td>
-                <td className="table-cell">
-                  <h2>Recovered</h2>
-                </td>
-                <td className="table-cell">
-                  <h2>Mortality Rate</h2>
-                </td>
-              </tr>
-              {tableData}
-            </tbody>
-          </table>
-        </div>
-      </React.Fragment>
-    );
-  }
-}
-function parseJson(response) {
-  return response.json();
+            {tableData}
+          </tbody>
+        </table>
+      </div>
+    </React.Fragment>
+  );
 }
 
+/**
+ *
+ * @param {Oject[]} this_data used to calculate the entire population of the country by adding up the individual populations of each province/territory.
+ * @returns integer value of the country's population
+ */
+function calculateCountryPop(this_data) {
+  let totalPop = 0;
+  for (const provinceInfo of this_data) {
+    if (provinceInfo.province !== "Repatriated") totalPop += provinceInfo.pop;
+  }
+
+  return totalPop;
+}
 export default TableStats;
