97fb0d4d493391cc568c4e2563e8d1ec6eb311fb
SunWukong97 <34043095+ThomasNLy@users.noreply.github.com>
2021-11-23 12:31:30 -0500

    App.js will now be the parent controller for api calls
    moved the api call from TableStats.jsx to App.js in order to better reuse its functionality as other components will need to interact with it in the future.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,86 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import React from "react";
+// import React from "react";
 import NavBar from "./components/NavBar.jsx";
 import HeroSection from "./components/HeroSection";
 import AboutSection from "./components/AboutSection";
-function App() {
-  return (
-    <React.Fragment>
-      <NavBar />
-      <HeroSection />
-      <AboutSection />
-    </React.Fragment>
-  );
+import React, { Component } from "react";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSet1: null,
+      dataSet2: null,
+      dataSet3: null,
+      isLoading: true,
+      date: null,
+    };
+  }
+
+  componentDidMount() {
+    let todayDate = new Date();
+    let yesterdayDate = new Date(todayDate);
+    yesterdayDate.setDate(yesterdayDate.getDate() - 1);
+    let formatDate = yesterdayDate.toISOString().slice(0, 10);
+
+    this.apiCall(formatDate);
+  }
+
+  apiCall(dateValue) {
+    const apiUrl1 = `https://api.opencovid.ca/summary?date=${dateValue}`;
+    const apiUrl2 = "https://api.opencovid.ca/other?stat=prov";
+    const apiUrl3 = `https://api.opencovid.ca/summary?loc=canada&date=${dateValue}`;
+    let apiUrls = [apiUrl1, apiUrl2, apiUrl3];
+
+    Promise.all(
+      apiUrls.map((urlIndex) =>
+        fetch(urlIndex).then((response) => this.parseJson(response))
+      )
+    ).then((data) => {
+      console.log("Success", data[0]);
+      console.log("Success", data[1]);
+      console.log("success", data[2]);
+
+      this.setState({
+        dataSet1: data[0].summary,
+        dataSet2: data[1].prov,
+        dataSet3: data[2].summary,
+        isLoading: false,
+        date: dateValue,
+      });
+    });
+  }
+
+  parseJson(response) {
+    return response.json();
+  }
+
+  dateSelection(date) {
+    this.apiCall(date);
+  }
+
+  render() {
+    let heroSection;
+    if (!this.state.isLoading && this.state.dataSet1.length !== 0) {
+      heroSection = (
+        <HeroSection
+          data1={this.state.dataSet1}
+          data2={this.state.dataSet2}
+          data3={this.state.dataSet3}
+          date={this.state.date}
+        />
+      );
+    } else {
+      heroSection = <p className="error">Data is currently Unavailable</p>;
+    }
+    return (
+      <React.Fragment>
+        <NavBar />
+        {heroSection}
+        <AboutSection />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
