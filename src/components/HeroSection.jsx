bb5fe852b7e708403fd29f9576533e43d8902d52
SunWukong97 <34043095+ThomasNLy@users.noreply.github.com>
2021-11-23 12:32:18 -0500

    will now receive props in order to pass onto tablestats component

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import TableStats from "./TableStats";
-function HeroSection() {
+function HeroSection(props) {
+  const { data1, data2, data3, date } = props;
   return (
     <React.Fragment>
-      <TableStats />
+      <TableStats data1={data1} data2={data2} data3={data3} date={date} />
     </React.Fragment>
   );
 }
