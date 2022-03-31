d8a4150fa8d915601aae71e4a97850c88f69b64e
SunWukong97 <34043095+ThomasNLy@users.noreply.github.com>
2022-03-31 18:29:51 -0400

    checks if number of people vaccinated doesn't exist and make it 0
    if the web api returns null for  number of people vaccinated than set set it to 0 people vaccinated.

--- src/components/StatsCard.jsx
+++ src/components/StatsCard.jsx
@@ -3,13 +3,14 @@ import CircleBar from "./CircleBar";
 import "./StatsCard.css";
 function StatsCard(props) {
   const { provinceName, fullyVacc, population } = props;
-  let percentageVacc = parseInt((fullyVacc / population) * 100);
+  let numVaccinated = fullyVacc === "NULL" ? 0 : fullyVacc;
+  let percentageVacc = parseInt((numVaccinated / population) * 100);
   return (
     <div className="card">
       <div className="card-content">
         <h2>{provinceName}</h2>
         <CircleBar size={150} percentage={percentageVacc} />
-        <p>{fullyVacc}</p>
+        <p>{numVaccinated}</p>
         <p>Fully vaccinated</p>
       </div>
     </div>
