95a03c83592612ad5d52db773dda02df5fc99afc
SunWukong97 <34043095+ThomasNLy@users.noreply.github.com>
2022-05-24 13:40:16 -0400

    reworked it to work with newly updated open covid api

--- src/components/StatsCardDeck.jsx
+++ src/components/StatsCardDeck.jsx
@@ -3,26 +3,24 @@ import StatsCard from "./StatsCard";
 import "./StatsCardDeck.css";
 
 function StatsCardDeck(props) {
-  const { data1, data2 } = props;
+  const { data1 } = props;
   let deckOfCards;
   deckOfCards = data1.map((provStats) => {
-    return data2.map((provInfo) => {
-      if (
-        provStats.region !== "Repatriated" &&
-        provStats.region === provInfo.code
-      ) {
-        return (
-          <StatsCard
-            key={provStats.region}
-            provinceName={provInfo.name}
-            fullyVacc={provStats.vaccine_administration_dose_2}
-            population={provInfo.population}
-          />
-        );
-      }
+    if (provStats.region !== null) {
+      return (
+        <StatsCard
+          key={provStats.region}
+          cardTitle={provStats.region}
+          data1={provStats.vaccine_administration_dose_3}
+          data2={provStats.vaccine_administration_total_doses}
+          text="Recieved dose 3"
+        />
+      );
+    } else {
       return null;
-    });
+    }
   });
+
   return (
     <React.Fragment>
       <h2>Vaccination Status</h2>
