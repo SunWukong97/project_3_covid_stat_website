9b39e2dc04fe6e6e9c11ec99b1006e83cdbbbc27
SunWukong97 <34043095+ThomasNLy@users.noreply.github.com>
2022-05-24 13:39:21 -0400

    Generalized the component to be useable for other projects
    Generalized the component so it can be reused to display other stats in the same format

--- src/components/StatsCard.jsx
+++ src/components/StatsCard.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import CircleBar from "./CircleBar";
 import "./StatsCard.css";
 function StatsCard(props) {
-  const { provinceName, fullyVacc, population } = props;
-  let numVaccinated = fullyVacc === "NULL" ? 0 : fullyVacc;
-  let percentageVacc = parseInt((numVaccinated / population) * 100);
+  const { cardTitle, data1, data2, text } = props;
+  let metric = data1 === "NULL" ? 0 : data1;
+  let percentage = parseInt((metric / data2) * 100);
   return (
     <div className="card">
       <div className="card-content">
-        <h2>{provinceName}</h2>
-        <CircleBar size={150} percentage={percentageVacc} />
-        <p>{numVaccinated}</p>
-        <p>Fully vaccinated</p>
+        <h2>{cardTitle}</h2>
+        <CircleBar size={150} percentage={percentage} />
+        <p>{metric}</p>
+        <p>{text}</p>
       </div>
     </div>
   );
